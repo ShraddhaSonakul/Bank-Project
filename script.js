0b9da55be832e2ba0edfe2d04c08b099f0c49c1a
ShraddhaSonakul <144772103+ShraddhaSonakul@users.noreply.github.com>
2024-03-03 18:55:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function a1(){
+    document.getElementById("drop1").style.display="block";
+}
+function a2(){
+    document.getElementById("drop1").style.display="none";
+}
+function b1(){
+    document.getElementById("drop2").style.display="block";
+}
+function b2(){
+    document.getElementById("drop2").style.display="none";
+}
+function c1(){
+    document.getElementById("drop3").style.display="block";
+}
+function c2(){
+    document.getElementById("drop3").style.display="none";
+}
+function d1(){
+    document.getElementById("drop4").style.display="block";
+}
+function d2(){
+    document.getElementById("drop4").style.display="none";
+}
